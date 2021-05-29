d5c100873d5fb64b160609dfe1246eb92ae2815d
Mark Thanadabouth <mark28ten@gmail.com>
2021-05-29 15:07:21 -0700

    site works. just have to get the while loop working so same pics dont show up

--- js/app.js
+++ js/app.js
@@ -12,9 +12,10 @@ let myContainer = document.querySelector('section');
 let myButton = document.querySelector('div');
 let imageOne = document.querySelector('section img:first-child');
 let imageTwo = document.querySelector('section img:nth-child(2)');
-// let imageThree = document.querySelector('section img:last-child');
+let imageThree = document.querySelector('section img:last-child');
 
-function Item(name, fileExtension = '.jpg') {
+// constructor
+function Item(name, fileExtension = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
   this.clicks = 0;
@@ -29,6 +30,18 @@ new Item('boots');
 new Item('breakfast');
 new Item('bubblegum');
 new Item('chair');
+new Item('cthulhu')
+new Item('dog-duck');
+new Item('dragon');
+new Item('pen');
+new Item('pet-sweep');
+new Item('scissors');
+new Item('shark');
+new Item('sweep', 'png');
+new Item('tauntaun');
+new Item('unicorn');
+new Item('water-can');
+new Item('wine-glass');
 
 
 function selectRandomProducttIndex() {
@@ -38,7 +51,7 @@ function selectRandomProducttIndex() {
 function renderRandomProducts() {
   let productOne = selectRandomProducttIndex();
   let productTwo = selectRandomProducttIndex();
-  // let productThree = selectRandomProducttIndex();
+  let productThree = selectRandomProducttIndex();
   while (productOne === productTwo) {
     productTwo = selectRandomProducttIndex();
   }
@@ -47,42 +60,46 @@ function renderRandomProducts() {
   imageOne.alt = allProducts[productOne].name;
   allProducts[productOne].views++;
 
-  imageTwo.src = allGoats[goatTwo].src;
-  imageTwo.alt = allGoats[goatTwo].name;
+  imageTwo.src = allProducts[productTwo].src;
+  imageTwo.alt = allProducts[productTwo].name;
   allProducts[productTwo].views++;
+
+  imageThree.src = allProducts[productThree].src;
+  imageThree.alt = allProducts[productThree].name;
+  allProducts[productThree].views++;
 }
 
 function handleProductClick(event) {
   if (event.target === myContainer) {
-    alert('click on an IMAGE please');
+    alert('Please click on an IMAGE to continue.');
   }
 
   clicks++;
   let clickedProduct = event.target.alt;
-  for (let i = 0; i < allGoats.length; i++) {
-    if (clickedProduct === allGoats[i].name) {
+  for (let i = 0; i < allProducts.length; i++) {
+    if (clickedProduct === allProducts[i].name) {
       allProducts[i].clicks++;
     }
   }
 
   renderRandomProducts();
-  
-  if(clicks === clicksAllowed){
+
+  if (clicks === clicksAllowed) {
     myContainer.removeEventListener('click', handleProductClick);
   }
 }
 
-function renderResults(){
+function renderResults() {
   let ul = document.querySelector('ul');
-  for(let i = 0; i < allProducts.length; i++){
+  for (let i = 0; i < allProducts.length; i++) {
     let li = document.createElement('li');
     li.textContent = `${allProducts[i].name} had ${allProducts[i].views} views and was clicked ${allProducts[i].clicks} times.`;
     ul.appendChild(li);
   }
 }
 
-function handleButtonClick(event){ //eslint-disable-line
-  if(clicks === clicksAllowed){
+function handleButtonClick(event) { //eslint-disable-line
+  if (clicks === clicksAllowed) {
     renderResults();
   }
 }
